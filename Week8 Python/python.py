2e1aa7f93274e68a263e97337d33faaadc3f00dd
Faku-tech <gadiniyandisa@gmail.com>
2025-06-30 21:45:59 +0200

    fetch commit

---
+++ Week8 Python/python.py
@@ -0,0 +1,36 @@
+#Numeric Data
+
+num = 3
+
+print(type(num))
+
+num2 = 3.14
+
+print(type(num2))
+
+# int num = 3
+# float num = 3
+
+#Variables
+
+my_variables = 10
+total_count = 0
+user = 'John'
+
+#Invalid
+second_variable = 10
+user_name =20
+
+#Operators
+
+#Addition (+)
+#Subtraction (-)
+#Multiplication(*)
+#Division(/)
+#Modulus(%)
+#Exponential(**)
+
+x= 10
+y = 2
+
+print(x+y)
