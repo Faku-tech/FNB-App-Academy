5c095e2c96b56f1002b2c79ffba8f255788e0e9e
Faku-tech <gadiniyandisa@gmail.com>
2025-06-30 21:21:44 +0200

    fetch commit

---
+++ Week7 Python/strings.py
@@ -0,0 +1,30 @@
+#strings
+
+message = """Bo's world
+is cool"""
+
+print(message)
+
+#Advanced concepts-strings
+
+
+message = 'Hello'
+
+print(message[0])
+
+print(message[1])
+
+print(message[-1])
+
+message = ' Hello '
+
+print(len(message))
+
+message = ' Hello world '
+
+print(message.strip()) # Remove leading and trailing whitespace
+print(message.lower()) # Convert all characters to lowercase
+print(message.split()) # Split the strings into a list based on the coma
+
+#upper method
+#replace method
